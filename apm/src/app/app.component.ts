0d54f784cb39e9bb7f0366492677a3e125a825ed
Mario Samperi <mariosamperi@marios-mbp.home>
2022-08-27 07:40:27 +0200

    created app component

--- apm/src/app/app.component.ts
+++ apm/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: "pm-root",
   template:` 
-  <div> <h1> {{pageTitle}} </h1></div>,
+  <div> <h1> {{pageTitle}} </h1></div>
     <div>My first Component<div>
   <div>
   `
 })
 export class AppComponent {
-  pageTitle = 'Acme Product MAnagement';
+  pageTitle = 'Acme Product Management';
 }
